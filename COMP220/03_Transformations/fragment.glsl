c7e6d31faf3817a7e7d2657ec39a9c255ea1be9d
Ed Powley <edpowley@gmail.com>
2016-10-05 12:23:35 +0100

    Added live coding template for COMP220_3

---
+++ COMP220/03_Transformations/fragment.glsl
@@ -0,0 +1,10 @@
+#version 330 core
+
+in vec3 colour;
+
+out vec3 fragmentColour;
+
+void main()
+{
+	fragmentColour = colour;
+}
---
+++ COMP220/03_Transformations/stdafx.h
@@ -0,0 +1,39 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+// C++ runtime header files
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+// SDL
+#include <SDL.h>
+
+// OpenGL
+#include <GL/glew.h>
+#include <gl/GL.h>
+
+// GLM
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
