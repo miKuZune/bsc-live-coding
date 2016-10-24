9def126317aeecd85bd68958143daca2a618757e
Ed Powley <edpowley@gmail.com>
2016-10-24 11:24:43 +0100

    OOP live coding exercise

--- COMP110/06_OOP/bouncing_balls.py
+++ COMP110/06_OOP/bouncing_balls.py
@@ -1,6 +1,67 @@
 import pygame
 import random
 
+
+class Ball:
+    def __init__(self):
+        # Choose a random position
+        self.pos_x = random.randrange(0, window_width)
+        self.pos_y = random.randrange(0, window_height)
+
+        # Choose a random speed
+        self.speed_x = random.randrange(-10, 10)
+        self.speed_y = random.randrange(-10, 10)
+
+        # Choose a random size
+        self.radius = random.randrange(2, 20)
+
+        # Choose a random colour
+        red = random.randrange(50, 255)
+        green = random.randrange(50, 255)
+        blue = random.randrange(50, 255)
+        self.colour = (red, green, blue)
+
+    def update(self):
+        self.pos_x += self.speed_x
+        self.pos_y += self.speed_y
+
+        # Bounce off the walls
+        if self.pos_x < 0 or self.pos_x > window_width:
+            self.speed_x = -self.speed_x
+
+        if self.pos_y < 0 or self.pos_y > window_height:
+            self.speed_y = -self.speed_y
+    
+    def draw(self):
+        print "This should never be called"
+
+
+class SquareBall(Ball):
+    def draw(self):
+        pos_x = self.pos_x
+        pos_y = self.pos_y
+        radius = self.radius
+        rect = (pos_x - radius, pos_y - radius, 2*radius, 2*radius)
+        pygame.draw.rect(screen, self.colour, rect)
+
+
+class CircleBall(Ball):
+    def draw(self):
+        pos_x = self.pos_x
+        pos_y = self.pos_y
+        radius = self.radius
+        pygame.draw.circle(screen, self.colour, (pos_x, pos_y), radius)
+
+
+class TriangleBall(Ball):
+    def draw(self):
+        pos_x = self.pos_x
+        pos_y = self.pos_y
+        radius = self.radius
+        points = [(pos_x - radius, pos_y + radius), (pos_x, pos_y - radius), (pos_x + radius, pos_y + radius)]
+        pygame.draw.polygon(screen, self.colour, points)
+
+
 # Initialise PyGame
 pygame.init()
 clock = pygame.time.Clock()
@@ -12,37 +73,14 @@ window_size = (window_width, window_height)
 screen = pygame.display.set_mode(window_size)
 
 # Initialise lists for ball data
-ball_pos_x = []
-ball_pos_y = []
-ball_speed_x = []
-ball_speed_y = []
-ball_radius = []
-ball_colour = []
-ball_shape = []
+balls = []
 
 # Create balls
 num_balls = 100
 for ball_index in xrange(num_balls):
-    # Choose a random position
-    ball_pos_x.append(random.randrange(0, window_width))
-    ball_pos_y.append(random.randrange(0, window_height))
-
-    # Choose a random speed
-    ball_speed_x.append(random.randrange(-10, 10))
-    ball_speed_y.append(random.randrange(-10, 10))
-
-    # Choose a random size
-    ball_radius.append(random.randrange(2, 20))
-
-    # Choose a random colour
-    red = random.randrange(50, 255)
-    green = random.randrange(50, 255)
-    blue = random.randrange(50, 255)
-    ball_colour.append((red, green, blue))
-
-    # Choose a random shape
-    shape = random.choice(["square", "triangle", "circle"])
-    ball_shape.append(shape)
+    shape = random.choice([SquareBall, TriangleBall, CircleBall])
+    new_ball = shape()
+    balls.append(new_ball)
 
 # Main loop
 running = True
@@ -54,31 +92,9 @@ while running:
     # Clear the screen to black
     screen.fill((0, 0, 0))
 
-    # Loop through the balls
-    for ball_index in xrange(num_balls):
-        # Update position
-        ball_pos_x[ball_index] += ball_speed_x[ball_index]
-        ball_pos_y[ball_index] += ball_speed_y[ball_index]
-
-        # Bounce off the walls
-        if ball_pos_x[ball_index] < 0 or ball_pos_x[ball_index] > window_width:
-            ball_speed_x[ball_index] = -ball_speed_x[ball_index]
-
-        if ball_pos_y[ball_index] < 0 or ball_pos_y[ball_index] > window_height:
-            ball_speed_y[ball_index] = -ball_speed_y[ball_index]
-
-        # Draw the ball
-        pos_x = ball_pos_x[ball_index]
-        pos_y = ball_pos_y[ball_index]
-        radius = ball_radius[ball_index]
-        if ball_shape[ball_index] == "square":
-            rect = (pos_x - radius, pos_y - radius, 2*radius, 2*radius)
-            pygame.draw.rect(screen, ball_colour[ball_index], rect)
-        elif ball_shape[ball_index] == "triangle":
-            points = [(pos_x - radius, pos_y + radius), (pos_x, pos_y - radius), (pos_x + radius, pos_y + radius)]
-            pygame.draw.polygon(screen, ball_colour[ball_index], points)
-        elif ball_shape[ball_index] == "circle":
-            pygame.draw.circle(screen, ball_colour[ball_index], (pos_x, pos_y), radius)
+    for ball in balls:
+        ball.update()
+        ball.draw()
 
     # Flip the display and regulate the frame rate
     pygame.display.flip()
