a0fa431f186dfd1f78ede613c6bfdab27fba0e2d
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2017-09-24 16:45:48 +0100

    Removed old COMP220 examples

--- COMP220/06_Lighting/Vertex.cpp
+++
@@ -1,8 +0,0 @@
-#include "stdafx.h"
-#include "Vertex.h"
-
-Vertex::Vertex(const glm::vec3& position, const glm::vec3& colour,
-	const glm::vec2& textureCoord, const glm::vec3& normal)
-	: m_position(position), m_colour(colour), m_textureCoord(textureCoord), m_normal(normal)
-{
-}
